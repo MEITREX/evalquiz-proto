8235d0eba35a908e7c044846b4552a81490ba358
st170001 <st170001@stud.uni-stuttgart.de>
2023-09-16 18:59:30 +0200

    Extension to mimetype mapping

--- shared/mimetype_resolver.py
+++ shared/mimetype_resolver.py
@@ -1,4 +1,5 @@
 import mimetypes
+from pathlib import Path
 from typing import Optional
 
 
@@ -21,5 +22,23 @@ class MimetypeResolver:
         if suffix == ".pptx":
             return "application/vnd.openxmlformats-officedocument.presentationml.presentation"
         else:
-            (type, _) = mimetypes.guess_type(suffix)
+            local_path = Path("test/misc" + suffix)
+            (type, _) = mimetypes.guess_type(local_path, strict=False)
             return type
+
+    @staticmethod
+    def fixed_guess_extension(mimetype: str) -> Optional[str]:
+        """Fixes the missing markdown type in Python's mimetypes library.
+
+        Args:
+            mimetype (str): Mimetype to guess extension for.
+
+        Returns:
+            Optional[str]: Extension with leading dot.
+        """
+        if mimetype == "text/markdown":
+            return ".md"
+        if mimetype == ".pptx":
+            return "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        else:
+            return mimetypes.guess_extension(mimetype, strict=False)
